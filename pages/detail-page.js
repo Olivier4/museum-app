ed3c466eab4a59d0a299dabe76564811e7bbbf70
Olivier <oliviervanthof@gmail.com>
2019-03-21 13:47:39 +0100

    get user input

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,3 +1,20 @@
 function submitComment() {
-    window.alert('Submitted')
-  }
+    const inputField = document.getElementById('name')
+    const name = inputField.value
+    const messageField =document.getElementById('msg')
+    const msg = messageField.value
+    console.log(msg)
+
+    const comment = document.createElement('section')
+    const h3 = document.createElement('h3')
+    const p = document.createElement('p')
+
+    h3.innerHTML = `${name} said:`
+    p.innerHTML = msg
+    comment.classList.add('comment')
+    comment.appendChild(h3)
+    comment.appendChild(p)
+
+  console.log(comment)
+}
+  
